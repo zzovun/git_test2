c14ad90e774606164e7b249b88f7fad408a95981
zzovun <zzovun@gmail.com>
2023-09-27 10:09:24 +0900

    화면 출력 방법 수정

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 	
 	String message = "안녕하세요";
 
-	public void say() {
-		System.out.println(message);
+	public String say() {
+		return message;
 	}
 }
--- src/git_test2/Main.java
+++ src/git_test2/Main.java
@@ -4,6 +4,6 @@ public class Main {
 	
 	public static void main(String[] args) {
 		Hello hello = new Hello();
-		hello.say();
+		System.out.println(hello.say());
 	}
 }
